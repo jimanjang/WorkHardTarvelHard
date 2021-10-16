c74f6ba4e3f4f0b6997dc73ea727b6a84689458c
jimanj <jangjiman@jangjiman-ui-MacBookPro.local>
2021-10-16 17:54:39 +0900

    add touchableopacity and textinput

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity, 
+  TextInput,
+} 
+from 'react-native';
+import { theme } from './color';
 
 export default function App() {
+  const[ working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text 
+          style={{ ... styles.btnText, color: working ? "white": theme.grey}}
+          >Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text 
+          style={{ ... styles.btnText, color: !working ? "white": theme.grey}}
+          >Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+        <TextInput 
+          returnKeyType="send"
+          placeholder={working ? "Add a To Do List": "where do you want to go?"}
+          style={styles.input} 
+          />
     </View>
   );
 }
@@ -14,8 +43,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
   },
+  header: {
+    justifyContent: "space-between",
+    flexDirection:"row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  input :{
+    backgroundColor: "white", 
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
+  }
 });
